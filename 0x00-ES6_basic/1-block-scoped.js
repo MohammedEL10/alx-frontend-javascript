b4e03e244269beba6576cbd6bb78e2101063383f
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-06-24 21:54:56 +0100

    ES6 basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,10 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    var task = true;
-    var task2 = false;
+    
   }
+  var task = true;
+  var task2 = false;
 
   return [task, task2];
 }
