f4a1b743a86111b12ac9741334cdfed37a404a28
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-06-26 07:01:42 +0100

    0x01-ES6_promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,7 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-    
+    return new Promise((body, firstName, lastName) => {
+        
+    })
 }
