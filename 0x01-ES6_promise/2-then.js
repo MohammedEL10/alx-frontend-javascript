d82eb0c32dd8918ffacc193971939c7e14c7caaf
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-06-26 19:29:31 +0100

    0x01-ES6_promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,12 @@
-export default function handleResponseFromAPI(promise) {
-    return promise
-    .then(() => ({status: 200, body: 'success'}))
-    .catch(() => new Error())
-    .finally(() => crossOriginIsolated.log('Got a response from the API'));
+function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success'
+    };
+  }).catch(() => {
+    console.error('An error occurred while fetching data from the API');
+    return new Error();
+  });
 }
