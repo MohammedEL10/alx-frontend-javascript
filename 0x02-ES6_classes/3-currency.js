9a97112787f064c2a1dde8b20077448ef886dd5f
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-06-27 23:59:53 +0100

    0x02-ES6_classe

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,4 +3,23 @@ class Currency {
         this._code = code;
         this._name = name;
     }
+    set code(code) {
+    if (typeof code !== 'number') {
+      throw new TypeError('code must be a number');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+  set name(name) {
+    if (typeof name !== 'string') {
+        throw new TypeError('name must be a string');
+    }
+    this._name = name;
+  }
+  displayFullCurrency() {
+    return `${this._code} (${this._name})`;
+  }
 }
