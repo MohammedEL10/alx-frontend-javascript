e134911c6823d3f4a2cff17036d4ef47526eead4
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-07-02 09:59:06 +0100

    0x03-ES6_data_manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,9 @@
-export default function getListStudentIds() {
-    return [
-        {firstName: 'Guillaume', id: 1, location: 'San Francisco'},
-        {firstName: 'James', id: 2, location: 'Columbia'},
-        {firstName: 'Serena', id: 5, location: 'San Francisco'}
-
-    ]
+export default function getListStudentIds(array) {
+    if (!array.isArray(array)) {
+        retrun [];
+    }
+    return array.map((a) => a.id)
 }
-const studentList = getListStudentIds();
-console.log(studentList);
+getListStudentIds("hello");
+getListStudentIds(getListStudents());
+
